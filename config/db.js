419caa4fa1c6cd1e3a248750be32feee55e013f6
Phan Công Minh <congminh13012k2@gmail.com>
2024-07-29 23:59:54 +0700

    Add files via upload

---
+++ config/db.js
@@ -0,0 +1,14 @@
+const mongoose = require("mongoose")
+
+// TMDT-NHOM7:mongodb-tmdt-nhom7
+async function connectDB(){
+    try{
+        await mongoose.connect("mongodb+srv://PhanCongMinh:123@cluster0.bwbooni.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+        //await mongoose.connect("mongodb+srv://TMDT-NHOM7:mongodb-tmdt-nhom7@tmdt-nhom7.5dpxvgh.mongodb.net/Ecommerce?retryWrites=true&w=majority&appName=TMDT-NHOM7")
+        console.log("Connect to DB successfully");
+    }catch(err){
+        console.log(err)
+    }
+}
+
+module.exports = connectDB
